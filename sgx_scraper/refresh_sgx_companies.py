cadd2663edd836c32ccdb69f41ca63692a57911e
Fawwaz Atha Rohmatullah <132468764+fawazatha@users.noreply.github.com>
2026-02-10 14:02:34 +0700

    feat: add refresh company lookup

--- sgx_scraper/refresh_sgx_companies.py
+++ sgx_scraper/refresh_sgx_companies.py
@@ -3,14 +3,15 @@ from sgx_scraper.config.settings import SUPABASE_CLIENT
 import json 
 import os 
 
-CACHE_PATH = 'data/sgx_companies.json'
+SGX_LOOKUP_PATH = 'data/sgx_companies.json'
+SGX_PATH = 'data/sgx_companies.json'
 
 def get_sgx_companies():
     try:
         response = (
             SUPABASE_CLIENT
             .table('sgx_companies')
-            .select('name, symbol')
+            .select('name, symbol', 'sector', 'sub_sector')
             .execute()
         )
         return response.data
@@ -20,11 +21,28 @@ def get_sgx_companies():
         return None
 
 
+def refresh_master_company_data_lookup():     
+    datas = get_sgx_companies()
+
+    sgx_lookup = {}
+    for data in datas: 
+        symbol = data.get('symbol') 
+        sgx_lookup[symbol] = data
+
+    sgx_lookup_list = [sgx_lookup]
+
+    os.makedirs(os.path.dirname(SGX_LOOKUP_PATH), exist_ok=True)
+    with open(SGX_LOOKUP_PATH, 'w', encoding='utf-8') as file:
+        json.dump(sgx_lookup_list, file, ensure_ascii=False, indent=2)
+        
+    print(f"Saved {len(sgx_lookup)} companies to data/sgx_companies_lookup.json")
+
+
 def refresh_master_company_data():     
     data = get_sgx_companies()
 
-    os.makedirs(os.path.dirname(CACHE_PATH), exist_ok=True)
-    with open(CACHE_PATH, 'w', encoding='utf-8') as file:
+    os.makedirs(os.path.dirname(SGX_PATH), exist_ok=True)
+    with open(SGX_PATH, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=2)
         
     print(f"Saved {len(data)} companies to data/sgx_companies.json")
@@ -32,3 +50,4 @@ def refresh_master_company_data():
 
 if __name__ == '__main__':
     refresh_master_company_data()
+    refresh_master_company_data_lookup()
